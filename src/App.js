3f1f00daa3f8e38e8bf223b67bea58dc059957fa
jovannedeljkovicatvss <its0925@atvssnis.edu.rs>
2025-11-25 10:12:58 +0100

    Add HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Intro from './pages/Intro';
 
 function App() {
   return (
-    <div className="App">
-      <Intro />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/gps-tracker-website" element={<Intro />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
